ada82a41e32c7007fac8873cd926b5b87ef17c30
riko-net <japman2400@gmail.com>
2021-02-26 14:25:37 +0100

    Status-404 exception added. But needs to display the message. Working on it.

--- src/main/java/com/pineapple/palapa/exception/UserNotFoundException.java
+++ src/main/java/com/pineapple/palapa/exception/UserNotFoundException.java
@@ -1,5 +1,7 @@
 package com.pineapple.palapa.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 public class UserNotFoundException extends RuntimeException {
     
     /**
@@ -9,5 +11,8 @@ public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(String message) {
         super(message);
+        throw new ResponseStatusException(
+            HttpStatus.NOT_FOUND, "entity not found"
+        );
     }
 }
