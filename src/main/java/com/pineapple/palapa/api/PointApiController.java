62cf4a1028ff8b1a20d6741bac342498cc60846f
riko-net <japman2400@gmail.com>
2021-04-30 21:14:11 +0200

    SQL queries

--- src/main/java/com/pineapple/palapa/api/PointApiController.java
+++ src/main/java/com/pineapple/palapa/api/PointApiController.java
@@ -1,38 +1,38 @@
-// package com.pineapple.palapa.api;
+package com.pineapple.palapa.api;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-// import com.pineapple.palapa.model.Point;
-// import com.pineapple.palapa.service.PointService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.pineapple.palapa.model.Point;
+import com.pineapple.palapa.service.PointService;
 
-// import java.util.List;
+import java.util.List;
 
-// @RestController
-// @RequestMapping("/api/cities") 
-// public class PointApiController {
+@RestController
+@RequestMapping("/api/points") 
+public class PointApiController {
 
-//     private final PointService pointService;
+    private final PointService pointService;
 
-//     public PointApiController(PointService pointService) {
-//         this.pointService = pointService;
-//     }
+    public PointApiController(PointService pointService) {
+        this.pointService = pointService;
+    }
 
-//     @GetMapping("/all")
-//     public ResponseEntity<List<Point>> getAllPoints() {
-//         List<Point> points = pointService.findAllPoints();
-//         return new ResponseEntity<>(points, HttpStatus.OK);
-//     }
+    @GetMapping("/all")
+    public ResponseEntity<List<Point>> getAllPoints() {
+        List<Point> points = pointService.findAllPoints();
+        return new ResponseEntity<>(points, HttpStatus.OK);
+    }
 
-//     @PostMapping("/add")
-//     public ResponseEntity<Point> addPoint(@RequestBody Point point) {
-//         Point newPoint = pointService.addPoint(point);
-//         return new ResponseEntity<>(newPoint, HttpStatus.CREATED);
-//     }
+    @PostMapping("/add")
+    public ResponseEntity<Point> addPoint(@RequestBody Point point) {
+        Point newPoint = pointService.addPoint(point);
+        return new ResponseEntity<>(newPoint, HttpStatus.CREATED);
+    }
 
-//     @DeleteMapping("/delete/{id}")
-//     public ResponseEntity<?> deletePoint(@PathVariable("id") Long id) {
-//         pointService.deletePoint(id);
-//         return new ResponseEntity<>(HttpStatus.OK);
-//     }
-// }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deletePoint(@PathVariable("id") Long id) {
+        pointService.deletePoint(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
--- src/sql/points.sql
+++ src/sql/points.sql
@@ -1,4 +1,13 @@
-SELECT p.name as `Point name`, l.name as `Location name`, p.latitude, p.longitude FROM db_palapa.vertice as v
-INNER JOIN db_palapa.location as l on l.id = v.location_id
-INNER JOIN db_palapa.point as p on p.id = v.point_id
-WHERE l.name = 'Mountain';
+DELIMITER //
+CREATE PROCEDURE `get_set_of_points_for_location` (
+    IN location_name VARCHAR(255)
+)
+BEGIN
+    SELECT p.name as `Point name`, l.name as `Location name`, p.latitude, p.longitude FROM db_palapa.vertice as v
+    INNER JOIN db_palapa.location as l on l.id = v.location_id
+    INNER JOIN db_palapa.point as p on p.id = v.point_id
+    WHERE l.name = location_name;
+END //
+DELIMITER ;
+
+CALL get_set_of_points_for_location('Water line');
