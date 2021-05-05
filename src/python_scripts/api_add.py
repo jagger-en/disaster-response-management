a31f9d1c2c4e370f271991507d4be3958300979b
riko-net <japman2400@gmail.com>
2021-05-05 23:36:42 +0200

    Python data is working

--- src/python_scripts/api_add.py
+++ src/python_scripts/api_add.py
@@ -1,12 +1,15 @@
 import requests
 import json
 import time
-from geo_data import geo_data
-from bridge_data import bridge_data
-from road_data import road_data
+import sys
 
 
-pointType_endpoint = "http://localhost:8080/api/pointTypes/all"
+points_add_endpoint = "http://localhost:8080/api/points/add"
+vertices_add_endpoint = "http://localhost:8080/api/vertices/add"
+
+points_all_endpoint = "http://localhost:8080/api/points/all"
+pointType_all_endpoint = "http://localhost:8080/api/pointTypes/all"
+locations_all_endpoint = "http://localhost:8080/api/locations/all"
 
 def send_to_endpoint(endpoint, payload):
     headers = {'content-type': 'application/json', 'Host': 'myhost'}
@@ -18,27 +21,50 @@ def access_endpoint(endpoint):
     r_dict_list = json.loads(r.text)
     return r_dict_list
 
-def get_pointType_id(point_type_name):
-    all_pointTypes = access_endpoint(pointType_endpoint)
-    return [pt for pt in all_pointTypes if pt["name"] == point_type_name][0]
+def filter_by_name_query(name, endpoint):
+    all_records = access_endpoint(endpoint)
+    return [pt for pt in all_records if pt["name"] == name][0]
+
+def check_status(status, msg):
+    if status == 201:
+        print(f"------SUCCESS------ {msg}")
+    else:
+        print(f"------ERROR------ {msg}")
 
 
 
-geo_features_list = [road_data["features"][0]]
 
-for idx, geo_f in enumerate(geo_features_list):
-    points_endpoint = "http://localhost:8080/api/points/add"
-    name = f"Sample-{idx}"
+json_file_path = sys.argv[1]
+location_name = sys.argv[2]
+point_type_name = sys.argv[3]
+with open(json_file_path) as f:
+  data_list = json.load(f)["features"]
+
+
+
+
+
+
+for idx, geo_f in enumerate(data_list):
+    point_name = f"Point-{idx}"
     longitude = geo_f["geometry"]["coordinates"][0]
     latitude = geo_f["geometry"]["coordinates"][1]
     height = 145
-    payload = {
-        "name": name,
+    point_payload = {
+        "name": point_name,
         "longitude": longitude,
         "latitude": latitude,
         "height": height,
-        "pointType": 7
+        "pointType": filter_by_name_query(point_type_name, pointType_all_endpoint)
     }
-    send_to_endpoint(points_endpoint, payload)
-    print(f"Wait...data-{idx} was inserted.")
+    result = send_to_endpoint(points_add_endpoint, point_payload)
+    check_status(result.status_code, f"P: {point_name}")
+
+    vertice_payload = {
+        "point": filter_by_name_query(point_name, points_all_endpoint),
+        "location": filter_by_name_query(location_name, locations_all_endpoint)
+    }
+    result = send_to_endpoint(vertices_add_endpoint, vertice_payload)
+    check_status(result.status_code, f'V: {vertice_payload["point"]["name"]} {vertice_payload["location"]["name"]}')
+    print()
 
