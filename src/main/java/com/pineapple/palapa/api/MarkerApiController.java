2f2760fe29d3ed715c48f1dcfda120b9968555eb
riko-net <japman2400@gmail.com>
2021-05-05 18:06:34 +0200

    Thabo: Dijkstra algorithm added

--- src/main/java/com/pineapple/palapa/api/MarkerApiController.java
+++ src/main/java/com/pineapple/palapa/api/MarkerApiController.java
@@ -2,6 +2,8 @@ package com.pineapple.palapa.api;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import com.pineapple.palapa.functions.ShortestPath;
 import com.pineapple.palapa.model.Marker;
 import com.pineapple.palapa.service.MarkerService;
 
@@ -20,6 +22,22 @@ public class MarkerApiController {
     @CrossOrigin
     @GetMapping("/all")
     public ResponseEntity<List<Marker>> getAllMarkers() {
+        try {
+            System.out.println("[::MESSAGE::] Finding ShortestPath...");
+            int graph[][] = new int[][] { 
+                    { 0, 2, 1, 0, 0, 0},
+                    { 2, 0, 7, 0, 8, 4},
+                    { 1, 7, 0, 7, 0, 3},
+                    { 0, 0, 7, 0, 8, 4},
+                    { 0, 8, 0, 8, 0, 5},
+                    { 0, 4, 3, 4, 5, 0} };
+            ShortestPath g = new ShortestPath();
+            g.algo_dijkstra(graph, 0);
+        } catch (Exception e) {
+            System.out.println("FAILED ALGORITHM");
+            System.out.println(e);
+        }
+
         List<Marker> markers = markerService.findAllMarkers();
         return new ResponseEntity<>(markers, HttpStatus.OK);
     }
