ce4208be2879883cff96b355a05f5e5d7b1db7cc
riko-net <japman2400@gmail.com>
2021-05-06 01:52:32 +0200

    SQL

--- src/sql/points.sql
+++ src/sql/points.sql
@@ -43,7 +43,17 @@ CALL get_set_of_points_for_location('Water line');
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_set_of_points_for_location`()
 BEGIN
-    SELECT p.name as `point_name`, l.name as `location_name`, p.latitude, p.longitude, p.id FROM db_palapa.vertice as v
+    SELECT 
+		p.name as `point_name`, 
+        l.name as `location_name`, 
+		p.latitude, 
+        p.longitude, 
+        pt.name as `point_type_name`,
+        p.id
+	FROM db_palapa.vertice as v
     INNER JOIN db_palapa.location as l on l.id = v.location_id
-    INNER JOIN db_palapa.point as p on p.id = v.point_id;
+    INNER JOIN db_palapa.point as p on p.id = v.point_id
+	INNER JOIN db_palapa.point_type as pt on pt.id = p.point_type_id;
 END
+
+
