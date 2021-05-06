7bd85f0ecfc5843ab0593c42561e899ef062f2c5
riko-net <japman2400@gmail.com>
2021-05-06 09:58:25 +0200

    SQL

--- src/sql/points.sql
+++ src/sql/points.sql
@@ -1,47 +1,10 @@
-DELIMITER //
-CREATE PROCEDURE `get_set_of_points_for_location` (
-    IN location_name VARCHAR(255)
-)
-BEGIN
-    SELECT p.name as `Point name`, l.name as `Location name`, p.latitude, p.longitude FROM db_palapa.vertice as v
-    INNER JOIN db_palapa.location as l on l.id = v.location_id
-    INNER JOIN db_palapa.point as p on p.id = v.point_id
-    WHERE l.name = location_name;
-END //
-DELIMITER ;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- CREATE PROCEDURE `get_set_of_points_for_location` (
+--     IN location_name VARCHAR(255)
+-- )
 
 
 DELIMITER //
-CREATE PROCEDURE `get_all_points` ()
-BEGIN
-    SELECT * FROM point;
-END //
-DELIMITER ;
-
-CALL get_set_of_points_for_location('Water line');
-
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `get_set_of_points_for_location`()
+CREATE PROCEDURE `get_set_of_points_for_location` ()
 BEGIN
     SELECT 
 		p.name as `point_name`, 
@@ -54,6 +17,29 @@ BEGIN
     INNER JOIN db_palapa.location as l on l.id = v.location_id
     INNER JOIN db_palapa.point as p on p.id = v.point_id
 	INNER JOIN db_palapa.point_type as pt on pt.id = p.point_type_id;
-END
+END //
+DELIMITER ;
 
+
+DELIMITER //
+CREATE PROCEDURE `get_all_points` ()
+BEGIN
+    SELECT * FROM point;
+END //
+DELIMITER ;
+
+
+
+
+
+
+    SELECT 
+		*
+	FROM db_palapa.vertice as v
+    INNER JOIN db_palapa.location as l on l.id = v.location_id
+    INNER JOIN db_palapa.point as p on p.id = v.point_id
+	INNER JOIN db_palapa.point_type as pt on pt.id = p.point_type_id;
+    
+    
+SELECT * FROM db_palapa.point as p INNER JOIN db_palapa.point_type as pt on pt.id = p.point_type_id;
 
