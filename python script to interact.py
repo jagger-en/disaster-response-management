563ed27dc82f58844875180c968ac89e1b7ee14f
riko-net <japman2400@gmail.com>
2021-05-06 07:40:58 +0200

    Names generator

--- python script to interact.py
+++ python script to interact.py
@@ -1,75 +1,16 @@
-from __future__ import absolute_import, division, print_function, \
-    unicode_literals
-import os
+import names
+import random
 
-from flask import Flask, g, jsonify, request, session, redirect
-from dotenv import load, get as getEnv
-from flask_openid import OpenID
-# from flask_login import login_user, logout_user, current_user, login_required
+def get_random_name():
+    person_name = names.get_full_name(gender='male')
+    if random.random() >= 0.5:
+        person_name = names.get_full_name(gender='female')
+    first_name = person_name.split(" ")[0]
+    last_name = person_name.split(" ")[1]
+    return {
+        "first_name": first_name,
+        "last_name": last_name
+    }
 
-from helper import emulate_ml_model
-
-load()
-
-app = Flask(__ http://localhost:8080/cities__)
-
-env = getEnv('ENV')
-
-if env:
-    app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = getEnv('SECRET')
-
-BASE_DIR = os.path.dirname((os.path.abspath(__file__)))
-# lm = LoginManager()
-# lm.init_app(app)
-oid = OpenID(app, os.path.join(BASE_DIR, 'tmp'), safe_roots=[])
-
-
-@app.before_request
-def lookup_current_user():
-    g.user = None
-    if 'openid' in session:
-        g.user = session['openid']
-        # g.user = User.query.filter_by(openid=openid).first()
-
-
-@app.route('/login', methods=['GET', 'POST'])
-@oid.loginhandler
-def login():
-    if g.user is not None:
-        return redirect(oid.get_next_url())
-    if request.method == 'POST':
-        openid = request.json.get('openid')
-        if openid:
-            return oid.try_login(openid, ask_for=['email', 'nickname'],
-                                ask_for_optional=['fullname'])
-    return jsonify({'error': 'No valid Open ID provider specified'}), 400
-
-
-@oid.after_login
-def set_user_session_login(resp):
-    session['openid'] = resp.identity_url
-    return redirect(oid.get_next_url())
-
-
-@app.route('/')
-def index():
-    print(session)
-    return 'API is working!'
-
-
-@app.route('/ml', methods=['POST'])
-def ml():
-    json_input = request.json
-    output_data = emulate_ml_model(json_input)
-    return jsonify({'data': output_data})
-
-
-@app.route('/logout')
-def logout():
-    session.pop('openid', None)
-    return redirect(oid.get_next_url())
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=getEnv('PORT', 8080))
+person_name = get_random_name()
+print(person_name)
