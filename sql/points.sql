c135d44ee0918696d1bac5b31f42f98e8f7d9437
jagger-en <japman2400@gmail.com>
2024-12-09 10:32:21 +0100

    Add summary stored procedure

--- sql/points.sql
+++ sql/points.sql
@@ -69,3 +69,45 @@ BEGIN
     INNER JOIN team ON team.id = misteam.team_id;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION get_summary_query()
+RETURNS TABLE (
+    id TEXT,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    job_title_name VARCHAR(255),
+    mission_name VARCHAR(255),
+    team_name VARCHAR(255),
+    point_name VARCHAR(255),
+    location_name VARCHAR(255),
+    latitude VARCHAR(255),
+    longitude VARCHAR(255),
+    point_type_name VARCHAR(255)
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        LEFT(UUID_GENERATE_V4()::text, 8) AS id,
+        per.first_name AS first_name,
+        per.last_name AS last_name,
+        jt.name AS job_title_name,
+        mis.name AS mission_name,
+        team.name AS team_name,
+        p.name AS point_name,
+        l.name AS location_name,
+        p.latitude AS latitude,
+        p.longitude AS longitude,
+        pt.name AS point_type_name
+    FROM vertice v
+    INNER JOIN location l ON l.id = v.location_id
+    INNER JOIN mission mis ON mis.location_id = l.id
+    INNER JOIN mission_team misteam ON misteam.mission_id = mis.id
+    INNER JOIN team ON team.id = misteam.team_id
+    INNER JOIN team_employee team_emp ON team_emp.team_id = team.id
+    INNER JOIN employee emp ON emp.id = team_emp.employee_id
+    INNER JOIN job_title jt ON jt.id = emp.job_title_id
+    INNER JOIN person per ON per.id = emp.person_id
+    INNER JOIN point p ON p.id = v.point_id
+    INNER JOIN point_type pt ON pt.id = p.point_type_id;
+END;
+$$ LANGUAGE plpgsql;
